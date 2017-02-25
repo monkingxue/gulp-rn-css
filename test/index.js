237ad3f5c11f4325a6ad110c02d944afe063ac1e
monkingxue <1360662499@qq.com>
2017-02-25 22:49:14 +0800

    修改 test 文件

--- test/index.js
+++ test/index.js
@@ -1,32 +1,26 @@
 var assert = require("assert");
 var es = require("event-stream");
 var File = require("vinyl");
-var RNTransformCSS = require("../");
+var RNTransformCSS = require("../index");
 
 describe("gulp-RNTransformCSS", function () {
-  describe("in streaming mode", function () {
+  describe("in buffer mode", function () {
 
-    it("转换 css style 字符串", function (done) {
+    it("to transform css to js object", function (done) {
 
-      // 创建伪文件
       var fakeFile = new File({
         contents: es.readArray([".a {b-a: 12px; .b{ xun: control } } .c {d: wer}"])
       });
 
-      // 创建一个 prefixer 流（stream）
       var streamer = RNTransformCSS();
 
-      // 将伪文件写入
       streamer.write(fakeFile);
 
-      // 等文件重新出来
       streamer.once("data", function (file) {
-        // 确保它以相同的方式出来
+
         assert(file.isBuffer());
 
-        // 缓存内容来确保它已经被处理过（加前缀内容）
         file.contents.pipe(es.wait(function (err, data) {
-          // 检查内
           assert.equal(file.contents.toString("utf8"), '"a":{"bA":"12px","b":{"xun":"control",}};"c":{"d":"wer",}');
           done();
         }));
